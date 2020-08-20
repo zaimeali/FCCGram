1b5fcd9304692cb5b00941318d8b7b8d264c8456
abdul-raffay <abdul.raffay@nytrotech.com>
2020-08-20 16:59:34 +0500

    logo and app name change in app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,8 +23,10 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand d-flex" href="{{ url('/') }}">
+                    <div><img src="/svg/fcc.svg" alt="fcc logo" class="pr-3" style="height: 20px; border-right: 1px solid #333;"></div>
+                    <div class="pl-3">freeCodeGram</div>
+                    {{-- {{ config('app.name', 'freeCodeGram') }} --}}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
