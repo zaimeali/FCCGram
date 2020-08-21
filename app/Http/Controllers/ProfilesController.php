dc98316b0bf2147b23f60b701dc0e3e95216c156
abdul-raffay <abdul.raffay@nytrotech.com>
2020-08-21 12:40:59 +0500

    Showung User data through dd()

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class ProfilesController extends Controller
 {
     public function index($user) 
     {
-        dd($user);
+        // dd($user);
+        // User::find($user);
+        // if i pass 1 it will retrieve the data but when i pass any other number which is not stored
+        //      it will show me the error.
+        dd(User::find($user));
         return view('home');
     }
 }
